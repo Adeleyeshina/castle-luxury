2d685b505c434906fc03c3cb958d705a6546e4d0
Shina Adeleye <adeleyeshina4@gmail.com>
2025-09-18 16:45:33 -0700

    AUTH

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,7 +2,6 @@
 import { FaBars } from 'react-icons/fa';
 import Logo from '../assets/images/Logo.png'
 import { Link } from 'react-router-dom';
-import { useAuthStore } from '../store/useAuthStore';
 
 interface TopbarProps {
   collapsed: boolean;
@@ -12,7 +11,7 @@ interface TopbarProps {
 }
 
 const Topbar: React.FC<TopbarProps> = ({  toggleCollapse, toggleMobile }) => {
-  const user = useAuthStore((s)=>s.user)
+
   return (
     <header className="flex items-center justify-between bg-white p-4 shadow sticky top-0 z-50">
       <button onClick={toggleMobile} className="text-primary md:hidden" aria-label="Toggle sidebar">
